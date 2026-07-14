f50df6faa917c6ad8c975d7d7e76683aec50d735
Nguyen Quang Minh <minhnq1@talent.apero.vn>
2026-07-14 13:33:39 +0700

    feat: restore patrol mode around song info box and fix clipping by increasing window padding

--- macos-menubar/Sources/macos-menubar/macos_menubar.swift
+++ macos-menubar/Sources/macos-menubar/macos_menubar.swift
@@ -289,100 +289,106 @@ struct PopoverView: View {
 }
 
 struct OnekoView: View {
-    @State private var catPos: CGPoint = CGPoint(x: 100, y: 100)
-    @State private var isMoving = false
-    @State private var sleepTick = 0
+    @State private var catPos: CGPoint = CGPoint(x: -16, y: -16)
+    @State private var direction: Direction = .right
+    @State private var sleepTick = 50
     @State private var tickCounter = 0
     @State private var frameNo = 25
     @State private var surpriseTick = 0
     
-    let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
+    enum Direction { case right, down, left, up }
+    
+    let timer = Timer.publish(every: 0.08, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        ZStack {
-            if let url = Bundle.module.url(forResource: "\(frameNo)", withExtension: "gif"),
-               let nsImage = NSImage(contentsOf: url) {
-                Image(nsImage: nsImage)
-                    .resizable()
-                    .frame(width: 32, height: 32)
-                    .position(catPos)
-                    .shadow(color: .black.opacity(0.3), radius: 2)
+        GeometryReader { geo in
+            ZStack {
+                if let url = Bundle.module.url(forResource: "\(frameNo)", withExtension: "gif"),
+                   let nsImage = NSImage(contentsOf: url) {
+                    Image(nsImage: nsImage)
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                        .position(catPos)
+                        .shadow(color: .black.opacity(0.3), radius: 2)
+                }
             }
-        }
-        .onReceive(timer) { _ in
-            tickCounter += 1
-            if let window = NSApp.windows.first(where: { $0 is FloatingLyricsWindow }) {
-                let mouseLoc = NSEvent.mouseLocation
-                let windowFrame = window.frame
-                
-                let localX = mouseLoc.x - windowFrame.minX
-                let localY = windowFrame.maxY - mouseLoc.y
-                
-                let targetX = max(16, min(windowFrame.width - 16, localX))
-                let targetY = max(16, min(windowFrame.height - 16, localY))
-                
-                let target = CGPoint(x: targetX, y: targetY)
-                let dx = target.x - catPos.x
-                let dy = target.y - catPos.y
-                let dist = sqrt(dx*dx + dy*dy)
-                
-                if surpriseTick > 0 {
-                    surpriseTick -= 1
-                    frameNo = 32
-                    return
-                }
-                
-                if dist > 30 {
-                    if !isMoving {
-                        surpriseTick = 8 // Surprise before moving
-                        isMoving = true
-                        sleepTick = 0
-                        return
-                    }
-                    
-                    let jdx = Double(dx)
-                    let jdy = Double(-dy)
-                    let javaTheta = atan2(jdy, jdx)
-                    
-                    if javaTheta >= -Double.pi/8 && javaTheta <= Double.pi/8 {
-                        frameNo = (frameNo == 5) ? 6 : 5 // right
-                    } else if javaTheta > Double.pi/8 && javaTheta < 3*Double.pi/8 {
-                        frameNo = (frameNo == 3) ? 4 : 3 // upper-right
-                    } else if javaTheta >= 3*Double.pi/8 && javaTheta <= 5*Double.pi/8 {
-                        frameNo = (frameNo == 1) ? 2 : 1 // up
-                    } else if javaTheta > 5*Double.pi/8 && javaTheta < 7*Double.pi/8 {
-                        frameNo = (frameNo == 15) ? 16 : 15 // upper-left
-                    } else if javaTheta >= 7*Double.pi/8 || javaTheta <= -7*Double.pi/8 {
-                        frameNo = (frameNo == 13) ? 14 : 13 // left
-                    } else if javaTheta > -7*Double.pi/8 && javaTheta < -5*Double.pi/8 {
-                        frameNo = (frameNo == 11) ? 12 : 11 // bottom-left
-                    } else if javaTheta >= -5*Double.pi/8 && javaTheta <= -3*Double.pi/8 {
-                        frameNo = (frameNo == 9) ? 10 : 9 // down
-                    } else {
-                        frameNo = (frameNo == 7) ? 8 : 7 // bottom-right
-                    }
-                    
-                    let speed: CGFloat = min(dist, 12.0)
-                    catPos.x += (dx / dist) * speed
-                    catPos.y += (dy / dist) * speed
-                } else {
-                    isMoving = false
-                    sleepTick += 1
-                    
-                    let slowTick = sleepTick / 4
-                    if slowTick < 10 {
-                        frameNo = (slowTick % 2 == 0) ? 31 : 25 // lick
-                    } else if slowTick < 20 {
-                        frameNo = (slowTick % 2 == 0) ? 27 : 28 // scratch
-                    } else if slowTick < 25 {
-                        frameNo = 26 // yawn
-                    } else {
-                        frameNo = ((sleepTick / 8) % 2 == 0) ? 29 : 30 // sleep slowly
-                    }
-                }
+            .onReceive(timer) { _ in
+                updateCat(size: geo.size)
             }
         }
         .allowsHitTesting(false)
+    }
+    
+    func updateCat(size: CGSize) {
+        tickCounter += 1
+        
+        if surpriseTick > 0 {
+            surpriseTick -= 1
+            frameNo = 32
+            return
+        }
+        
+        if sleepTick > 0 {
+            sleepTick -= 1
+            let slowTick = sleepTick / 4
+            if sleepTick > 80 {
+                frameNo = (slowTick % 2 == 0) ? 31 : 25 // lick
+            } else if sleepTick > 40 {
+                frameNo = (slowTick % 2 == 0) ? 27 : 28 // scratch
+            } else if sleepTick > 30 {
+                frameNo = 26 // yawn
+            } else {
+                frameNo = ((sleepTick / 8) % 2 == 0) ? 29 : 30 // sleep slowly
+            }
+            
+            if sleepTick == 0 {
+                surpriseTick = 4
+            }
+            return
+        }
+        
+        let speed: CGFloat = 8
+        
+        switch direction {
+        case .right:
+            frameNo = (frameNo == 5) ? 6 : 5 // Right frames
+            catPos.x += speed
+            if catPos.x >= size.width + 16 {
+                catPos.x = size.width + 16
+                direction = .down
+                maybeSleep()
+            }
+        case .down:
+            frameNo = (frameNo == 9) ? 10 : 9 // Down frames
+            catPos.y += speed
+            if catPos.y >= size.height + 16 {
+                catPos.y = size.height + 16
+                direction = .left
+                maybeSleep()
+            }
+        case .left:
+            frameNo = (frameNo == 13) ? 14 : 13 // Left frames
+            catPos.x -= speed
+            if catPos.x <= -16 {
+                catPos.x = -16
+                direction = .up
+                maybeSleep()
+            }
+        case .up:
+            frameNo = (frameNo == 1) ? 2 : 1 // Up frames
+            catPos.y -= speed
+            if catPos.y <= -16 {
+                catPos.y = -16
+                direction = .right
+                maybeSleep()
+            }
+        }
+    }
+    
+    func maybeSleep() {
+        if Int.random(in: 0...2) == 0 {
+            sleepTick = 100
+        }
     }
 }
 
@@ -468,11 +474,10 @@ struct FloatingLyricsView: View {
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: alignment)
-        .padding(20)
+        .padding(40)
         .animation(.spring(response: 0.4, dampingFraction: 0.8), value: state.currentLyricIndex)
         .animation(.easeInOut(duration: 0.4), value: state.isTop)
         .animation(.easeInOut(duration: 0.4), value: state.isLeft)
-        .overlay(OnekoView())
     }
     
     @ViewBuilder
@@ -535,6 +540,7 @@ struct FloatingLyricsView: View {
             .padding(12)
             .background(VisualEffectView().cornerRadius(12).opacity(0.9))
             .shadow(color: .black.opacity(0.5), radius: 5, x: 0, y: 2)
+            .overlay(OnekoView())
         }
     }
     
